3f8a975e04177c83f5e712bc6ef413ff073d872a
RinorSefa <rinorsefa@hotmail.com>
2020-04-15 17:00:04 +0200

    PlayerResource class created

---
+++ src/main/java/com/footm/informationservice/resources/PlayerResource.java
@@ -0,0 +1,11 @@
+package com.footm.informationservice.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("player")
+@Produces(MediaType.APPLICATION_JSON)
+public class PlayerResource {
+}
