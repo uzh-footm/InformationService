e62c92accf34a34ffccbbaddb5cd55364f235aa3
RinorSefa <rinorsefa@hotmail.com>
2020-04-15 19:13:57 +0200

    getPlayer method created

--- src/main/java/com/footm/informationservice/db/PlayerDao.java
+++ src/main/java/com/footm/informationservice/db/PlayerDao.java
@@ -1,6 +1,16 @@
 package com.footm.informationservice.db;
 
 import com.footm.informationservice.api.Player;
+import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
+import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
 
-public class PlayerDao {
+public interface PlayerDao {
+
+    @SqlQuery("select id,name,age,photo,nationality,overall,club," +
+            "value,wage,preferredfoot,skillmoves,workrate," +
+            "position,jerseynumber,height,weight " +
+            "from player where id == :id ")
+    @RegisterBeanMapper(Player.class)
+    Player getPlayer(@Bind("id") int id);
 }
