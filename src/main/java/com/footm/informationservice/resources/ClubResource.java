29742ef0240def79c9b768e82430e67e9c69fa7e
RinorSefa <rinorsefa@hotmail.com>
2020-04-16 12:48:40 +0200

    Club Resource class with method getListClubs created

---
+++ src/main/java/com/footm/informationservice/resources/ClubResource.java
@@ -0,0 +1,29 @@
+package com.footm.informationservice.resources;
+
+import com.footm.informationservice.api.Club;
+import com.footm.informationservice.db.ClubDao;
+import com.footm.informationservice.db.PlayerDao;
+import org.jdbi.v3.core.Jdbi;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("clubs")
+@Produces(MediaType.APPLICATION_JSON)
+public class ClubResource {
+
+    private Jdbi jdbdi;
+
+    public ClubResource(Jdbi jdbdi){
+        this.jdbdi=jdbdi;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Club> getListClubs(){
+        return jdbdi.withExtension(ClubDao.class, dao -> dao.getListClubs());
+    }
+}
