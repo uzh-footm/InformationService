728e567befb7ff40e4183d876f22ae472bf5c61d
RinorSefa <rinorsefa@hotmail.com>
2020-04-16 12:22:23 +0200

    PlayerSkills and PlayerPositions API implementation

--- src/main/java/com/footm/informationservice/resources/PlayerResource.java
+++ src/main/java/com/footm/informationservice/resources/PlayerResource.java
@@ -1,6 +1,8 @@
 package com.footm.informationservice.resources;
 
 import com.footm.informationservice.api.Player;
+import com.footm.informationservice.api.PlayerPositions;
+import com.footm.informationservice.api.PlayerSkills;
 import com.footm.informationservice.db.PlayerDao;
 import org.jdbi.v3.core.Jdbi;
 
@@ -22,5 +24,19 @@ public class PlayerResource {
     @Produces(MediaType.APPLICATION_JSON)
     public Player getPlayer(@PathParam("playerId") int id){
         return jdbdi.withExtension(PlayerDao.class, dao -> dao.getPlayer(id));
+    }
+
+    @GET
+    @Path("/{playerId}/skills")
+    @Produces(MediaType.APPLICATION_JSON)
+    public PlayerSkills getPlayerSkills(@PathParam("playerId") int id){
+        return jdbdi.withExtension(PlayerDao.class, dao -> dao.getPlayerSkills(id));
+    }
+
+    @GET
+    @Path("/{playerId}/positions")
+    @Produces(MediaType.APPLICATION_JSON)
+    public PlayerPositions getPlayerPositions(@PathParam("playerId") int id){
+        return jdbdi.withExtension(PlayerDao.class, dao -> dao.getPlayerPositions(id));
     }
 }
